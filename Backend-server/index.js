19ff94eb36e6e1592c717d00e4aba68f972d1149
Pavanaditya09 <155769454+Pavanaditya09@users.noreply.github.com>
2025-01-04 20:29:15 +0530

    Update index.js

--- Backend-server/index.js
+++ Backend-server/index.js
@@ -10,13 +10,7 @@ const PORT = process.env.PORT;
 // Your OpenWeatherMap API Key
 const API_KEY = process.env.API_KEY;
 
-app.use(cors(
-  {
-    origin: {"https://weather-climate-xi.vercel.app"},
-    methods:["GET"],
-    credentials: true
-  }
-  ));
+app.use(cors());
 
 app.get("/api/:path", async (req, res) => {
   const { path } = req.params;
