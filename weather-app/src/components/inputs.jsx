b97d8cf96641525a57d2d1bf04a97a147d59f657
Pavanaditya09 <155769454+Pavanaditya09@users.noreply.github.com>
2025-01-10 14:30:27 +0530

    Update inputs.jsx

--- weather-app/src/components/inputs.jsx
+++ weather-app/src/components/inputs.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState} from "react";
 import { BiSearch, BiCurrentLocation } from "react-icons/bi";
 const Inputs = ({ setQuery, setUnits }) => {
   const [city, setCity] = useState("");
